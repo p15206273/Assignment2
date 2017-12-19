16a755903b5134492a288a6585b5afdc1c794ab5
Kacper Kozak <P15206273@LEC-ADMIN.dmu.ac.uk>
2017-12-19 13:04:22 +0000

    modified imageslider.js

--- js/imageslider.js
+++ js/imageslider.js
@@ -3,12 +3,12 @@ carousel();
 
 function carousel() {
     var i;
-    var x = document.getElementsByClassName("mySlides");
+    var x = document.getElementsByClassName("images");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none"; 
     }
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1} 
     x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 2000); // Change image every 2 seconds
+    setTimeout(carousel, 4000); // Change image every 2 seconds
 }
